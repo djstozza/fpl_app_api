0c71fb7d08357b8977b663e054cb4db0416e9576
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2018-02-16 12:09:14 +1100

    Sort rounds in controller

--- app/controllers/api/v1/rounds_controller.rb
+++ app/controllers/api/v1/rounds_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RoundsController < ApplicationController
   # GET /rounds
   def index
-    respond_with(RoundSerializer.new(Round.all).serializable_hash[:data])
+    respond_with(RoundSerializer.new(Round.all.sort).serializable_hash[:data])
   end
 end
