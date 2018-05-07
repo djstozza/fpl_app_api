28444a10b3714d50e2668303e56cc06918bc5ab0
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2018-05-08 08:33:23 +1000

    Cater for two fixtures per round

--- app/decorators/fpl_team_list_decorator.rb
+++ app/decorators/fpl_team_list_decorator.rb
@@ -29,6 +29,9 @@ class FplTeamListDecorator < ApplicationDecorator
         :team_h_id,
         :team_a_id,
         :event_points,
+        :stats,
+        :finished,
+        :finished_provisional,
         :news,
         'fixtures.id AS fixture_id',
         'fpl_team_lists.round_id AS round_id',
@@ -36,20 +39,23 @@ class FplTeamListDecorator < ApplicationDecorator
         'opponents.id AS opponent_id',
         :team_h_difficulty,
         :team_a_difficulty,
-      ).map do |hash|
+      ).each_with_index.map do |hash, i|
+        hash['i'] = i
         fixture_history =
           Player.find(hash['player_id']).player_fixture_histories.find do |history|
-            history['fixture'] == hash['fixture_id']
+            history['round'] == hash['round_id'] && history['opponent_team'] == hash['opponent_id']
           end
 
         if fixture_history
           hash['minutes'] = fixture_history['minutes']
           hash['fixture_points'] = fixture_history['total_points']
+          hash['event_points'] = score_calculator(hash)
         end
 
         home_fixture = hash['team_id'] == hash['team_h_id']
 
         hash['leg'] = home_fixture ? 'H' : 'A'
+        hash['fixture'] = "#{hash['opponent_short_name']} (#{hash['leg']})"
 
         hash['advantage'] =
           if home_fixture
@@ -67,9 +73,10 @@ class FplTeamListDecorator < ApplicationDecorator
   end
 
   def grouped_list_position_arr
-    list_position_arr
-      .group_by { |h| h['role'] }
-      .each_with_object({}) { |(k, v), h| h[k] = v.group_by { |g| g['singular_name_short'] } }
+    arr = non_duplicates_arr + duplicates_arr
+    arr.group_by { |h| h['role'] }.each_with_object({}) do |(k, v), h|
+      h[k] = v.group_by { |g| g['singular_name_short'] }
+    end
   end
 
   def waiver_picks_arr
@@ -115,7 +122,7 @@ class FplTeamListDecorator < ApplicationDecorator
 
   def all_in_players_tradeable
     Player
-      .order(ict_index: :desc)
+      .order(total_points: :desc)
       .joins(:team)
       .joins(:position)
       .joins('JOIN fpl_teams_players ON fpl_teams_players.player_id = players.id')
@@ -123,7 +130,10 @@ class FplTeamListDecorator < ApplicationDecorator
       .joins('JOIN leagues ON fpl_teams.league_id = leagues.id')
       .joins('JOIN fpl_team_lists ON fpl_team_lists.fpl_team_id = fpl_teams.id')
       .joins('JOIN rounds ON fpl_team_lists.round_id = rounds.id')
-      .joins('JOIN list_positions ON list_positions.player_id = players.id')
+      .joins(
+        'JOIN list_positions ON list_positions.player_id = players.id AND ' \
+        'list_positions.fpl_team_list_id = fpl_team_lists.id'
+      )
       .where(leagues: { id: fpl_team.league_id }, fpl_team_lists: { round_id: round_id })
       .where.not(fpl_team_lists: { id: id })
       .pluck_to_hash(
@@ -146,13 +156,18 @@ class FplTeamListDecorator < ApplicationDecorator
   end
 
   def tradeable_players
-    players
-      .order(position_id: :desc, ict_index: :desc)
+    Player
+      .order(position_id: :desc, total_points: :desc)
       .joins(:team)
       .joins(:position)
       .joins('JOIN fpl_teams_players ON fpl_teams_players.player_id = players.id')
       .joins('JOIN fpl_teams ON fpl_teams_players.fpl_team_id = fpl_teams.id')
       .joins('JOIN fpl_team_lists ON fpl_team_lists.fpl_team_id = fpl_teams.id')
+      .joins(
+        'JOIN list_positions ON list_positions.player_id = players.id AND ' \
+        'list_positions.fpl_team_list_id = fpl_team_lists.id'
+      )
+      .where(fpl_team_lists: { id: id })
       .pluck_to_hash(
         :id,
         'fpl_teams.id AS fpl_team_id',
@@ -188,5 +203,28 @@ class FplTeamListDecorator < ApplicationDecorator
       s: 'fa fa-gavel',
       u: 'fa fa-times-circle',
     }
+  end
+
+  def non_duplicates_arr
+    list_position_arr.group_by { |h| h['player_id'] }.select { |k, v| v.size == 1 }.values.flatten
+  end
+
+  def duplicates_arr
+   list_position_arr
+    .group_by { |h| h['player_id'] }
+    .select { |k, v| v.size > 1 }
+    .map do |_k, v|
+      v[0]['fixture'] = v[0]['fixture'] + ', ' + v[1]['fixture']
+      v[0]['fixture_points'] += v[1]['fixture_points']
+      v[0]
+    end
+  end
+
+  def score_calculator(hash)
+    return hash['fixture_points'] if hash['finished'] || !hash['started']
+    bps_arr = hash.dig(:stats, :bps)
+    hash['fixture_points'] += 3 if bps_arr.first['element'] == hash['id']
+    hash['fixture_points'] += 2 if bps_arr.second['element'] == hash['id']
+    hash['fixture_points'] += 1 if bps_arr.third['element'] == hash['id']
   end
 end
