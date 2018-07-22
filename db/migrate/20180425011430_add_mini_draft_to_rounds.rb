b6cce0dc9860f6346ab41ea55169e7d3aa0fad43
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2018-07-23 09:02:57 +1000

    Add readme

--- db/migrate/20180425011430_add_mini_draft_to_rounds.rb
+++ db/migrate/20180425011430_add_mini_draft_to_rounds.rb
@@ -3,8 +3,8 @@ class AddMiniDraftToRounds < ActiveRecord::Migration[5.1]
     return if column_exists? :rounds, :mini_draft, :boolean
     add_column :rounds, :mini_draft, :boolean
     rounds = Round.order(:deadline_time)
-    rounds.where('deadline_time > ?', Round.summer_mini_draft_deadline).first.update(mini_draft: true)
-    rounds.where('deadline_time > ?', Round.winter_mini_draft_deadline).first.update(mini_draft: true)
+    rounds.find_by('deadline_time > ?', Round.summer_mini_draft_deadline + 3.days).update(mini_draft: true)
+    rounds.find_by('deadline_time > ?', Round.winter_mini_draft_deadline + 3.days).update(mini_draft: true)
   end
 
   def down
