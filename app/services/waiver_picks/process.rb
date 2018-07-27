6d244a9937e76252d0859cb0d3e58ff826d53c2a
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2018-07-28 08:50:30 +1000

    Refactor waiver pick processing

--- app/services/waiver_picks/process.rb
+++ app/services/waiver_picks/process.rb
@@ -4,10 +4,9 @@ class WaiverPicks::Process < ApplicationInteraction
   def execute
     return if Time.now < round.deadline_time - 1.day
     League.active.each do |league|
-      waiver_groups(league).each do |_k, waiver_group|
-        waiver_group_sorted_by_fpl_team(waiver_group).each do |waiver_pick|
-          compose(WaiverPicks::Approve, waiver_pick: waiver_pick)
-        end
+      pick_number_arr = league.waiver_picks.where(round: round).pluck(:pick_number).uniq
+      pick_number_arr.each do |pick_number|
+        waiver_group(league, pick_number).each { |waiver_pick| compose(WaiverPicks::Approve, waiver_pick: waiver_pick) }
       end
 
       league.waiver_picks.where(round: round).pending.update_all(status: 'declined')
@@ -16,13 +15,12 @@ class WaiverPicks::Process < ApplicationInteraction
 
   private
 
-  def waiver_groups(league)
-    league.waiver_picks.where(round: round).group_by { |pick| pick.pick_number }.sort
-  end
-
-  def waiver_group_sorted_by_fpl_team(waiver_group)
-    waiver_group.sort do |a, b|
-      b.fpl_team.rank <=> a.fpl_team.rank
-    end
+  def waiver_group(league, pick_number)
+    league.waiver_picks
+      .pending
+      .joins(:fpl_team_list)
+      .joins('JOIN fpl_teams ON fpl_team_lists.fpl_team_id = fpl_teams.id')
+      .where(round: round, pick_number: pick_number)
+      .order('fpl_team_lists.overall_rank DESC, fpl_team_lists.rank DESC, fpl_teams.mini_draft_pick_number ASC')
   end
 end
--- spec/services/waiver_picks/process_spec.rb
+++ spec/services/waiver_picks/process_spec.rb
@@ -13,23 +13,35 @@ RSpec.describe WaiverPicks::Process do
     out_player_1 = FactoryBot.create(:player, :fwd)
     out_player_2 = FactoryBot.create(:player, :fwd)
     out_player_3 = FactoryBot.create(:player, :fwd)
+    out_player_4 = FactoryBot.create(:player, :fwd)
+    out_player_5 = FactoryBot.create(:player, :fwd)
 
-    fpl_team_1 = FactoryBot.create(:fpl_team, league: league, rank: 1)
-    fpl_team_list_1 = FactoryBot.create(:fpl_team_list, fpl_team: fpl_team_1, round: round)
-    list_position_1 = FactoryBot.create(:list_position, :fwd, player: out_player_1, fpl_team_list: fpl_team_list_1)
+    fpl_team_1 = FactoryBot.create(:fpl_team, league: league, rank: 1, mini_draft_pick_number: 5)
+    fpl_team_list_1 = FactoryBot.create(:fpl_team_list, fpl_team: fpl_team_1, round: round, rank: 1, overall_rank: 1)
+    FactoryBot.create(:list_position, :fwd, player: out_player_1, fpl_team_list: fpl_team_list_1)
     fpl_team_1.players << out_player_1
 
-    fpl_team_2 = FactoryBot.create(:fpl_team, league: league, rank: 2)
-    fpl_team_list_2 = FactoryBot.create(:fpl_team_list, fpl_team: fpl_team_2, round: round)
-    list_position_2 = FactoryBot.create(:list_position, :fwd, player: out_player_2, fpl_team_list: fpl_team_list_2)
+    fpl_team_2 = FactoryBot.create(:fpl_team, league: league, rank: 2, mini_draft_pick_number: 4)
+    fpl_team_list_2 = FactoryBot.create(:fpl_team_list, fpl_team: fpl_team_2, round: round, rank: 2, overall_rank: 2)
+    FactoryBot.create(:list_position, :fwd, player: out_player_2, fpl_team_list: fpl_team_list_2)
     fpl_team_2.players << out_player_2
 
-    fpl_team_3 = FactoryBot.create(:fpl_team, league: league, rank: 3)
-    fpl_team_list_3 = FactoryBot.create(:fpl_team_list, fpl_team: fpl_team_3, round: round)
+    fpl_team_3 = FactoryBot.create(:fpl_team, league: league, rank: 2, mini_draft_pick_number: 3)
+    fpl_team_list_3 = FactoryBot.create(:fpl_team_list, fpl_team: fpl_team_3, round: round, rank: 3, overall_rank: 2)
     list_position_3 = FactoryBot.create(:list_position, :fwd, player: out_player_3, fpl_team_list: fpl_team_list_3)
     fpl_team_3.players << out_player_3
 
-    league.players += [out_player_1, out_player_2, out_player_3]
+    fpl_team_4 = FactoryBot.create(:fpl_team, league: league, rank: 2, mini_draft_pick_number: 2)
+    fpl_team_list_4 = FactoryBot.create(:fpl_team_list, fpl_team: fpl_team_4, round: round, rank: 3, overall_rank: 2)
+    list_position_4 = FactoryBot.create(:list_position, :fwd, player: out_player_4, fpl_team_list: fpl_team_list_4)
+    fpl_team_4.players << out_player_4
+
+    fpl_team_5 = FactoryBot.create(:fpl_team, league: league, rank: 5, mini_draft_pick_number: 1)
+    fpl_team_list_5 = FactoryBot.create(:fpl_team_list, fpl_team: fpl_team_5, round: round, rank: 5, overall_rank: 5)
+    list_position_5 = FactoryBot.create(:list_position, :fwd, player: out_player_5, fpl_team_list: fpl_team_list_5)
+    fpl_team_5.players << out_player_5
+
+    league.players += [out_player_1, out_player_2, out_player_3, out_player_4, out_player_5]
 
     waiver_pick_1 = FactoryBot.create(
       :waiver_pick,
@@ -65,20 +77,20 @@ RSpec.describe WaiverPicks::Process do
       :waiver_pick,
       round: round,
       league: league,
-      fpl_team_list: fpl_team_list_1,
-      out_player: out_player_1,
-      in_player: in_player_2,
-      pick_number: 2,
+      fpl_team_list: fpl_team_list_4,
+      out_player: out_player_4,
+      in_player: in_player_1,
+      pick_number: 1,
     )
 
     waiver_pick_5 = FactoryBot.create(
       :waiver_pick,
       round: round,
       league: league,
-      fpl_team_list: fpl_team_list_2,
-      out_player: out_player_2,
-      in_player: in_player_3,
-      pick_number: 2,
+      fpl_team_list: fpl_team_list_5,
+      out_player: out_player_5,
+      in_player: in_player_1,
+      pick_number: 1,
     )
 
     waiver_pick_6 = FactoryBot.create(
@@ -88,45 +100,90 @@ RSpec.describe WaiverPicks::Process do
       fpl_team_list: fpl_team_list_2,
       out_player: out_player_2,
       in_player: in_player_2,
+      pick_number: 2,
+    )
+
+    waiver_pick_7 = FactoryBot.create(
+      :waiver_pick,
+      round: round,
+      league: league,
+      fpl_team_list: fpl_team_list_3,
+      out_player: out_player_3,
+      in_player: in_player_2,
+      pick_number: 2,
+    )
+
+    waiver_pick_8 = FactoryBot.create(
+      :waiver_pick,
+      round: round,
+      league: league,
+      fpl_team_list: fpl_team_list_4,
+      out_player: out_player_4,
+      in_player: in_player_2,
+      pick_number: 2,
+    )
+
+    waiver_pick_9 = FactoryBot.create(
+      :waiver_pick,
+      round: round,
+      league: league,
+      fpl_team_list: fpl_team_list_2,
+      out_player: out_player_2,
+      in_player: in_player_3,
+      pick_number: 3,
+    )
+
+    waiver_pick_10 = FactoryBot.create(
+      :waiver_pick,
+      round: round,
+      league: league,
+      fpl_team_list: fpl_team_list_3,
+      out_player: out_player_3,
+      in_player: in_player_3,
       pick_number: 3,
     )
 
     described_class.run!
 
-    # waiver_pick_1, waiver_pick_2 and waiver_pick_3 all have a pick_number of 1
-    # waiver_pick_3 is processed first because fpl_team_3 has the lowest rank (3) - gets approved
-    # waiver_pick_2 is processed next because fpl_team_2 has the next highest rank (2)
-    #   - gets declined because in_player_1 was already incorporated into fpl_team_3 when waiver_pick_3 was approved
-    # waiver_pick_1 is processed next because fpl_team_1 hash the highest rank (1)
-    #   - gets declined because in_player_1 was already incorporated into fpl_team_3 when waiver_pick_3 was approved
-    expect(waiver_pick_1.reload.declined?).to be_truthy
-    expect(waiver_pick_2.reload.declined?).to be_truthy
-    expect(waiver_pick_3.reload.approved?).to be_truthy
+    # waiver_picks 1-5 have a pick_number of 1 and are trying to trade in in_player_1
+    # waiver_pick_5 is processed first because fpl_team_list_5 has the lowest overall rank (5) - gets approved
+    # waiver_picks 1-4 are declined since in_player_1 is taken
 
-    # waiver_pick_4 and waiver_pick_5 both have a pick_number of 2
-    # waiver_pick_5 gets processed first because fpl_team_2 rank > fpl_team_1 rank - gets approved
-    # waiver_pick_4 also gets approved
-    expect(waiver_pick_4.reload.approved?).to be_truthy
-    expect(waiver_pick_5.reload.approved?).to be_truthy
+    # waiver_picks 6-8 have a pick_number of 2 and are trying to trade in in_player_2
+    # waiver_pick_8 is processed first:
+    # - While fpl_team_listss 2-4 have the same overall rank (2), waiver_pick 6 gets processed last
+    #   since fpl_team_list_2 has the highest round rank (2)
+    # - While fpl_team_lists 3-4 have the same round rank (3), fpl_team_4 has the higher mini_draft_pick_number (2)
+    #   and is therefore processed first - gets approved.
+    # waiver_picks 6 and 7 are both declined since in_player_2 is taken
 
-    # waiver_pick_6 has a pick_number of 3
-    # waiver_pick_6 gets declined because in_player_2 was already incorporated into fpl_team_1 when waiver_pick_4
-    #   was approved
-    expect(waiver_pick_6.reload.declined?).to be_truthy
+    # waiver_picks 9-10 both have a pick_number of 3 and are trying to trade in in_player_3
+    # waiver_pick_10 is processed first since fpl_team_list_3 has a lower round rank (3) - gets approved
 
-    expect(fpl_team_1.players.reload).to contain_exactly(in_player_2)
-    expect(fpl_team_list_1.players.reload).to contain_exactly(in_player_2)
-    expect(list_position_1.reload.player).to eq(in_player_2)
+    expect(WaiverPick.approved).to contain_exactly(waiver_pick_5.reload, waiver_pick_8.reload, waiver_pick_10.reload)
+    expect(WaiverPick.declined).to contain_exactly(
+      waiver_pick_1.reload,
+      waiver_pick_2.reload,
+      waiver_pick_3.reload,
+      waiver_pick_4.reload,
+      waiver_pick_6.reload,
+      waiver_pick_7.reload,
+      waiver_pick_9.reload,
+    )
 
-    expect(fpl_team_2.players.reload).to contain_exactly(in_player_3)
-    expect(fpl_team_list_2.players.reload).to contain_exactly(in_player_3)
-    expect(list_position_2.reload.player).to eq(in_player_3)
+    expect(fpl_team_5.players.reload).to contain_exactly(in_player_1)
+    expect(fpl_team_list_5.players.reload).to contain_exactly(in_player_1)
+    expect(list_position_5.reload.player).to eq(in_player_1)
 
-    expect(fpl_team_3.players.reload).to contain_exactly(in_player_1)
-    expect(fpl_team_list_3.players.reload).to contain_exactly(in_player_1)
-    expect(list_position_3.reload.player).to eq(in_player_1)
+    expect(fpl_team_4.players.reload).to contain_exactly(in_player_2)
+    expect(fpl_team_list_4.players.reload).to contain_exactly(in_player_2)
+    expect(list_position_4.reload.player).to eq(in_player_2)
 
-    expect(league.players.reload).to contain_exactly(in_player_1, in_player_2, in_player_3)
+    expect(fpl_team_3.players.reload).to contain_exactly(in_player_3)
+    expect(fpl_team_list_3.players.reload).to contain_exactly(in_player_3)
+    expect(list_position_3.reload.player).to eq(in_player_3)
+
+    expect(league.reload.players).to contain_exactly(in_player_1, in_player_2, in_player_3, out_player_1, out_player_2)
   end
 
   it 'does not process waiver picks prior to the waiver_pick cut_off time' do
