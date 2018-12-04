6578bd9d39efa88b4f02fcc53d73cc52f058ceee
Daniel Sztolcman <daniel.sztolcman@gmail.com>
2018-12-05 01:40:25 +1100

    Refactor player fixture histories

--- app/decorators/player_decorator.rb
+++ app/decorators/player_decorator.rb
@@ -4,57 +4,64 @@ class PlayerDecorator < ApplicationDecorator
   end
 
   def players_hash
-    joins(:position).pluck_to_hash(
-      :id,
-      :first_name,
-      :last_name,
-      :photo,
-      :status,
-      :code,
-      :news,
-      :chance_of_playing_next_round,
-      :chance_of_playing_this_round,
-      :in_dreamteam,
-      :dreamteam_count,
-      :form,
-      :total_points,
-      :event_points,
-      :points_per_game,
-      :minutes,
-      :goals_scored,
-      :assists,
-      :clean_sheets,
-      :goals_conceded,
-      :own_goals,
-      :penalties_saved,
-      :penalties_missed,
-      :yellow_cards,
-      :red_cards,
-      :saves,
-      :bonus,
-      :bps ,
-      :influence,
-      :creativity,
-      :threat,
-      :ict_index,
-      :open_play_crosses,
-      :big_chances_created,
-      :clearances_blocks_interceptions,
-      :recoveries,
-      :key_passes,
-      :tackles,
-      :winning_goals,
-      :dribbles,
-      :fouls,
-      :errors_leading_to_goal,
-      :offside,
-      :position_id,
-      :team_id,
-      :singular_name_short,
-    ).map do |hash|
-      hash['status'] = status_class_hash[hash['status'].to_sym]
-      hash
-    end
+    joins(:position)
+      .joins(
+        "LEFT JOIN LATERAL JSONB_ARRAY_ELEMENTS (players.player_fixture_histories) fixture_histories ON " \
+        "(fixture_histories ->> 'element')::INT = players.id"
+      )
+      .group('players.id', 'singular_name_short')
+      .distinct
+      .pluck_to_hash(
+        :id,
+        :first_name,
+        :last_name,
+        :photo,
+        :status,
+        :code,
+        :news,
+        :chance_of_playing_next_round,
+        :chance_of_playing_this_round,
+        :in_dreamteam,
+        :dreamteam_count,
+        :form,
+        "SUM((fixture_histories ->> 'total_points')::INT) AS total_points",
+        :event_points,
+        :points_per_game,
+        "SUM((fixture_histories ->> 'minutes')::INT) AS minutes",
+        "SUM((fixture_histories ->> 'goals_scored')::INT) AS goals_scored",
+        "SUM((fixture_histories ->> 'assists')::INT) AS assists",
+        "SUM((fixture_histories ->> 'clean_sheets')::INT) AS clean_sheets",
+        "SUM((fixture_histories ->> 'goals_conceded')::INT) AS goals_conceded",
+        "SUM((fixture_histories ->> 'own_goals')::INT) AS own_goals",
+        "SUM((fixture_histories ->> 'penalties_saved')::INT) AS penalties_saved",
+        "SUM((fixture_histories ->> 'penalties_missed')::INT) AS penalties_missed",
+        "SUM((fixture_histories ->> 'yellow_cards')::INT) AS yellow_cards",
+        "SUM((fixture_histories ->> 'red_cards')::INT) AS red_cards",
+        "SUM((fixture_histories ->> 'saves')::INT) AS saves",
+        "SUM((fixture_histories ->> 'bonus')::INT) AS bonus",
+        "SUM((fixture_histories ->> 'bps')::INT) AS bps",
+        :influence,
+        :creativity,
+        :threat,
+        :ict_index,
+        :open_play_crosses,
+        "SUM((fixture_histories ->> 'big_chances_created')::INT) AS big_chances_created",
+        "SUM((fixture_histories ->> 'clearances_blocks_interceptions')::INT) AS clearances_blocks_interceptions",
+        "SUM((fixture_histories ->> 'recoveries')::INT) AS recoveries",
+        "SUM((fixture_histories ->> 'key_passes')::INT) AS key_passes",
+        "SUM((fixture_histories ->> 'winning_goals')::INT) AS winning_goals",
+        "SUM((fixture_histories ->> 'tackles')::INT) AS tackles",
+        "SUM((fixture_histories ->> 'dribbles')::INT) AS dribbles",
+        "SUM((fixture_histories ->> 'fouls')::INT) AS fouls",
+        "SUM((fixture_histories ->> 'errors_leading_to_goal')::INT) AS errors_leading_to_goal",
+        "SUM((fixture_histories ->> 'offside')::INT) AS offside",
+        :position_id,
+        :team_id,
+        :singular_name_short,
+      ).map do |hash|
+        hash['status'] = status_class_hash[hash['status'].to_sym]
+        hash
+      end
   end
 
 
--- app/services/players/populate_history.rb
+++ app/services/players/populate_history.rb
@@ -6,36 +6,9 @@ class Players::PopulateHistory < ApplicationInteraction
       player_fixture_histories: response['history'],
       player_past_histories: response['history_past']
     )
-
-    return if response['history']
-
-    player_stats_arr.each do |stat|
-      player.update(
-        stat =>  history_json['history'].inject(0) { |sum, fixtue_history| sum + fixtue_history[stat] }
-      )
-    end
   end
 
   private
-
-  def player_stats_arr
-    %w(
-      open_play_crosses
-      big_chances_created
-      clearances_blocks_interceptions
-      recoveries
-      key_passes
-      tackles
-      winning_goals
-      dribbles
-      fouls
-      errors_leading_to_goal
-      big_chances_missed
-      offside
-      attempted_passes
-      target_missed
-    )
-  end
 
   def response
     HTTParty.get("https://fantasy.premierleague.com/drf/element-summary/#{player.id}")
